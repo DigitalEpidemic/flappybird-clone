6e2634eb2477a78cd7eb01f03dae2dbbd7483f36
Jeff Polasz <jeff_polasz@hotmail.com>
2017-11-23 21:34:56 -0500

    Added physics

--- core/src/com/jeffpolasz/flappyclone/states/PlayState.java
+++ core/src/com/jeffpolasz/flappyclone/states/PlayState.java
@@ -3,17 +3,18 @@ package com.jeffpolasz.flappyclone.states;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.jeffpolasz.flappyclone.FlappyClone;
+import com.jeffpolasz.flappyclone.sprites.Bird;
 
 /**
  * Created by Jeff on 2017-11-23.
  */
 
 public class PlayState extends State{
-    private Texture bird;
+    private Bird bird;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
-        bird = new Texture("bird.png");
+        bird = new Bird(50, 100);
         cam.setToOrtho(false, FlappyClone.WIDTH/2, FlappyClone.HEIGHT/2);
     }
 
@@ -24,14 +25,15 @@ public class PlayState extends State{
 
     @Override
     public void update(float dt) {
-
+        handleInput();
+        bird.update(dt);
     }
 
     @Override
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
-        sb.draw(bird, 50, 50);
+        sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
         sb.end();
     }
 
