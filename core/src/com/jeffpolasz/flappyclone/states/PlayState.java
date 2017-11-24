2b2d10ef4ccece26d2a9b538679b67db5499a23a
Jeff Polasz <jeff_polasz@hotmail.com>
2017-11-23 22:09:40 -0500

    Added movement to the Tubes

--- core/src/com/jeffpolasz/flappyclone/states/PlayState.java
+++ core/src/com/jeffpolasz/flappyclone/states/PlayState.java
@@ -3,6 +3,7 @@ package com.jeffpolasz.flappyclone.states;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
 import com.jeffpolasz.flappyclone.FlappyClone;
 import com.jeffpolasz.flappyclone.sprites.Bird;
 import com.jeffpolasz.flappyclone.sprites.Tube;
@@ -12,16 +13,25 @@ import com.jeffpolasz.flappyclone.sprites.Tube;
  */
 
 public class PlayState extends State{
+    private static final int TUBE_SPACING = 125;
+    private static final int TUBE_COUNT = 4;
+
     private Bird bird;
     private Texture bg;
-    private Tube tube;
+
+    private Array<Tube> tubes;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50, 300);
         cam.setToOrtho(false, FlappyClone.WIDTH/2, FlappyClone.HEIGHT/2);
         bg = new Texture("bg.png");
-        tube = new Tube(100);
+
+        tubes = new Array<Tube>();
+
+        for (int i = 1; i <= TUBE_COUNT; i++) {
+            tubes.add(new Tube(i*(TUBE_SPACING + Tube.TUBE_WIDTH)));
+        }
     }
 
     @Override
@@ -35,6 +45,15 @@ public class PlayState extends State{
     public void update(float dt) {
         handleInput();
         bird.update(dt);
+        cam.position.x = bird.getPosition().x + 80;
+
+        for (Tube tube : tubes) {
+            if (cam.position.x - (cam.viewportWidth/2) > tube.getPosTopTube().x + tube.getTopTube().getWidth()) {
+                tube.reposition(tube.getPosTopTube().x + ((Tube.TUBE_WIDTH + TUBE_SPACING) * TUBE_COUNT));
+            }
+        }
+
+        cam.update();
     }
 
     @Override
@@ -43,8 +62,10 @@ public class PlayState extends State{
         sb.begin();
         sb.draw(bg, cam.position.x - cam.viewportWidth/2, 0);
         sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
-        sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
-        sb.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
+        for (Tube tube : tubes) {
+            sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
+            sb.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
+        }
         sb.end();
     }
 
