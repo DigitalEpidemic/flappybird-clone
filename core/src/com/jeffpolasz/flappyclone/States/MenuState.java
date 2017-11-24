45667afdb2e9692684da6089fbe557f243feb7e3
Jeff Polasz <jeff_polasz@hotmail.com>
2017-11-23 21:12:21 -0500

    Render background and playBtn

--- core/src/com/jeffpolasz/flappyclone/States/MenuState.java
+++ core/src/com/jeffpolasz/flappyclone/States/MenuState.java
@@ -1,14 +1,21 @@
 package com.jeffpolasz.flappyclone.States;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.jeffpolasz.flappyclone.FlappyClone;
 
 /**
  * Created by Jeff on 2017-11-23.
  */
 
 public class MenuState extends State {
+    private Texture background;
+    private Texture playBtn;
+
     public MenuState(GameStateManager gsm) {
         super(gsm);
+        background = new Texture("bg.png");
+        playBtn = new Texture("playBtn.png");
     }
 
     @Override
@@ -23,6 +30,9 @@ public class MenuState extends State {
 
     @Override
     public void render(SpriteBatch sb) {
-
+        sb.begin();
+        sb.draw(background, 0, 0, FlappyClone.WIDTH, FlappyClone.HEIGHT);
+        sb.draw(playBtn, FlappyClone.WIDTH/2 - playBtn.getWidth()/2, FlappyClone.HEIGHT/2);
+        sb.end();
     }
 }
