224f51476ea3949bd6d064c04418a996b38d160b
Jeff Polasz <jeff_polasz@hotmail.com>
2017-11-24 00:12:08 -0500

    Added background music and flap sound effect

--- core/src/com/jeffpolasz/flappyclone/FlappyClone.java
+++ core/src/com/jeffpolasz/flappyclone/FlappyClone.java
@@ -2,6 +2,7 @@ package com.jeffpolasz.flappyclone;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.jeffpolasz.flappyclone.states.GameStateManager;
@@ -14,11 +15,17 @@ public class FlappyClone extends ApplicationAdapter {
 
 	private GameStateManager gsm;
 	private SpriteBatch batch;
+
+	private Music music;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		gsm = new GameStateManager();
+		music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+		music.setLooping(true);
+		music.setVolume(0.1f);
+		music.play();
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		gsm.push(new MenuState(gsm));
 	}
@@ -28,5 +35,11 @@ public class FlappyClone extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		gsm.update(Gdx.graphics.getDeltaTime());
 		gsm.render(batch);
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
+		music.dispose();
 	}
 }
