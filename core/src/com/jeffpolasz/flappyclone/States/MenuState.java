59de3d9e801037b807436ccf5d6aa9d17867d824
Jeff Polasz <jeff_polasz@hotmail.com>
2017-11-23 21:02:06 -0500

    Added MenuState

---
+++ core/src/com/jeffpolasz/flappyclone/States/MenuState.java
@@ -0,0 +1,28 @@
+package com.jeffpolasz.flappyclone.States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by Jeff on 2017-11-23.
+ */
+
+public class MenuState extends State {
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+}
